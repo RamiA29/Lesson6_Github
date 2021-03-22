53f4227ae0985f0be7d1cb5d04e958d41cdb5e4d
RamiA29 <79037965+RamiA29@users.noreply.github.com>
2021-03-22 12:30:21 +0200

    Add files via upload

---
+++ Lesson6_Github/Selenium.py
@@ -0,0 +1,21 @@
+
+from selenium import webdriver
+driver = webdriver.Chrome(r'C:\Users\rami\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\chromedriver')
+print(driver.get('http://www.walla.co.il'))
+
+#print(driver.get('https://google.com'))
+#q = driver.find_element_by_name('q')
+#q.send_keys("http://www.walla.co.il")
+#btnk = driver.find_element_by_name('btnK')
+#btnk.submit()
+
+
+#print(driver.refresh())
+#print(driver.back())
+#print(driver.forward())
+#print(driver.close())
+#rq = driver.title
+#rs = driver.window_handles
+#print(rs)
+
+
